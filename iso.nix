369ae4dc0192d9505f89989b3df9c6ff0d6b1f6f
Antonio Gelameris <toniogela89@gmail.com>
2026-02-16 14:37:30 +0100

    wip

--- iso.nix
+++ iso.nix
@@ -8,6 +8,15 @@ let
   );
   compat = import sources.flake-compat;
   disko = compat { src = sources.disko; };
+  command = ''
+    echo "\e[1;32mClone your config then run[0m"
+    echo "\e[1;32m  \`disko --mode destroy,format,mount <disko.nix>\`\e[0m"
+    echo "\e[1;32mMove the config in /mnt/etc/nixos with \e[0m"
+    echo "\e[1;32m  \`sudo cp -r <cloned-folder>/* /etc/nixos\`\e[0m"
+    echo "\e[1;32mAnd then \`nixos-install\`\e[0m"
+    echo "\e[1;32mTo set a password for a user\e[0m"
+    echo "\e[1;32m \`nixos-enter --root /mnt -c 'passwd <user>'\`\e[0m"
+  '';
   # channel = import (nixpkgs + "/nixos/modules/installer/cd-dvd/channel.nix");
 in
 (nixos {
@@ -17,18 +26,13 @@ in
       # <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
     ];
 
-    programs.bash.interactiveShellInit = ''
-      echo "\e[1;32mClone your config then run[0m"
-      echo "\e[1;32m  `disko --mode destroy,format,mount <disko.nix>\e[0m"
-      echo "\e[1;32mMove the config in /mnt/etc/nixos with \e[0m"
-      echo "\e[1;32m  `sudo cp -r <cloned-folder>/* /etc/nixos`\e[0m"
-      echo "\e[1;32mAnd then `nixos-install`"
-    '';
+    programs.bash.interactiveShellInit = command;
 
     environment.systemPackages = [
       pkgs.neovim
       disko.outputs.packages.x86_64-linux.disko
       pkgs.git
+      (pkgs.writeShellScriptBin "instructions" "${command}")
     ];
   };
 }).config.system.build.isoImage
