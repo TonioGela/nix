05a1793f754fcd8d6e1dca5d824989c1a608e8fd
Antonio Gelameris <toniogela89@gmail.com>
2026-04-13 22:49:10 +0200

    yazi

--- home-manager/yazi/default.nix
+++ home-manager/yazi/default.nix
@@ -1,4 +1,12 @@
 { pkgs, ... }:
+let
+  what-size-plugin = pkgs.fetchFromGitHub {
+    owner = "pirafrank";
+    repo = "what-size.yazi";
+    rev = "main";
+    hash = "sha256-7q/45TopqbojNRvYDmP9+hgSGPmiyLHBcV051qpOB2Y=";
+  };
+in
 {
   programs.yazi = {
     enable = true;
@@ -9,35 +17,22 @@
       full-border = pkgs.yaziPlugins.full-border;
       smart-enter = pkgs.yaziPlugins.smart-enter;
       chmod = pkgs.yaziPlugins.chmod;
+      what-size = what-size-plugin;
     };
     initLua = ''
-      require("full-border"):setup {
-        -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-        type = ui.Border.ROUNDED,
-      }
-      Status:children_add(function(self)
-        local h = self._current.hovered
-        if h and h.link_to then
-          return " -> " .. tostring(h.link_to)
-        else
-          return ""
-        end
-      end, 3300, Status.LEFT)
-      Status:children_add(function()
-        local h = cx.active.current.hovered
-        if not h or ya.target_family() ~= "unix" then
-          return ""
-        end
-
-        return ui.Line {
-          ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
-          ":",
-          ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
-          " ",
-        }
-      end, 500, Status.RIGHT)
+      -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+      require("full-border"):setup { type = ui.Border.ROUNDED }
+      -- Hide StatusBar and Header
+      local old_layout = Tab.layout
+      Status.redraw = function() return {} end
+      Header.redraw = function() return {} end
+      Tab.layout = function(self, ...)
+        self._area = ui.Rect { x = self._area.x, y = self._area.y - 1, w = self._area.w, h = self._area.h + 2 }
+        return old_layout(self, ...)
+      end
     '';
     settings = {
+      mgr.linemode = "size";
 
       # opener.image = [
       #   {
@@ -86,6 +81,14 @@
         {
           run = "plugin mount";
           on = [ "M" ];
+        }
+        {
+          run = "plugin what-size";
+          on = [
+            "."
+            "s"
+          ];
+          desc = "Calc size of selection or cwd";
         }
       ];
     };
