4fb84b2d2ce498aaf289edb48bd30d25d7c72e76
Antonio Gelameris <toniogela89@gmail.com>
2026-05-19 21:45:51 +0200

    wip

--- modules/home-manager/zathura/default.nix
+++ modules/home-manager/zathura/default.nix
@@ -1,7 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   home = {
     packages = [ pkgs.zathura ];
     file.".config/zathura/zathurarc".source = ./zathurarc;
+  };
+
+  xdg.mimeApps = lib.mkIf pkgs.stdenv.isLinux {
+    enable = true;
+    defaultApplications = {
+      "application/pdf" = "org.pwmt.zathura.desktop";
+    };
   };
 }
