68d0f113a0247a1d277c20f4aa5493e71594dbc4
Antonio Gelameris <toniogela89@gmail.com>
2026-05-22 15:27:57 +0200

    wip

--- modules/nixos/power.nix
+++ modules/nixos/power.nix
@@ -9,4 +9,18 @@
   services.upower.enable = true;
   systemd.sleep.extraConfig = "HibernateDelaySec=5m";
   services.power-profiles-daemon.enable = true;
+
+  environment.etc."systemd/system-sleep/pre-hibernate-drop-cache" = {
+    mode = "0755";
+    text = ''
+      #!/bin/sh
+      if [ "$1" = "pre" ] && [ "$2" = "hibernate" ]; then
+        logger -t pre-hibernate-drop-cache "starting: $(free -m | awk '/^Mem:/{printf "%sMB free %sMB cached", $4, $6}')"
+        sync
+        echo 3 > /proc/sys/vm/drop_caches
+        echo 0 > /sys/power/image_size
+        logger -t pre-hibernate-drop-cache "done: $(free -m | awk '/^Mem:/{printf "%sMB free %sMB cached", $4, $6}')"
+      fi
+    '';
+  };
 }
