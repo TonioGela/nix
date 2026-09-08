1cb640ab443a7a727becb984e9e82881d0ff7829
Antonio Gelameris <toniogela89@gmail.com>
2026-09-08 09:40:40 +0200

    wip

--- modules/home-manager/todo.nix
+++ modules/home-manager/todo.nix
@@ -2,6 +2,16 @@
 let
   todoDir = "${config.home.homeDirectory}/.todo";
 
+  todoConfig = pkgs.writeText "todo-config" ''
+    export TODO_DIR="${todoDir}"
+    export TODO_FILE="$TODO_DIR/todo.txt"
+    export DONE_FILE="$TODO_DIR/done.txt"
+    export REPORT_FILE=/dev/null
+    export TODOTXT_DEFAULT_ACTION=ls
+    export TODOTXT_AUTO_ARCHIVE=1
+    export TODOTXT_VERBOSE=0
+  '';
+
   todo = pkgs.writeShellApplication {
     name = "todo";
     runtimeInputs = [
@@ -13,7 +23,7 @@ let
       git pull --rebase --quiet || echo "todo: pull failed, working offline" >&2
 
       status=0
-      env TODOTXT_VERBOSE=0 todo.sh "$@" || status=$?
+      env TODOTXT_CFG_FILE=${todoConfig} todo.sh "$@" || status=$?
 
       git add --all
       if ! git diff --cached --quiet; then
@@ -27,13 +37,4 @@ let
 in
 {
   home.packages = [ todo ];
-
-  home.file.".todo/config".text = ''
-    export TODO_DIR="${todoDir}"
-    export TODO_FILE="$TODO_DIR/todo.txt"
-    export DONE_FILE="$TODO_DIR/done.txt"
-    export REPORT_FILE="$TODO_DIR/report.txt"
-    export TODOTXT_DEFAULT_ACTION=ls
-    export TODOTXT_AUTO_ARCHIVE=0
-  '';
 }
